1998d1da07b9f5b05c5024a1d61ac1c3425a673a
Mongark <mongarkprogrammer@gmail.com>
2022-09-09 01:34:54 -0400

    :art: Improved structure of Status component

--- src/Components/Status/Status.tsx
+++ src/Components/Status/Status.tsx
@@ -4,10 +4,28 @@ import React from 'react'
 export default function Status(props: any) {
     return (
         <div>
-            <Select value={ props.value }>
-                <MenuItem value='Vacant'>Vacant</MenuItem>
-                <MenuItem value='Booked'>Booked</MenuItem>
-                <MenuItem value='In Use'>In Use</MenuItem>
+            <Select
+                variant='filled'
+                color='secondary'
+                value={ props.value }
+                >
+                    <MenuItem
+                        value='Vacant'
+                        >
+                            Vacant
+                    </MenuItem>
+
+                    <MenuItem
+                        value='Booked'
+                        >
+                            Booked
+                    </MenuItem>
+
+                    <MenuItem
+                        value='In Use'
+                        >
+                            In Use
+                    </MenuItem>
             </Select>
         </div>
     )
