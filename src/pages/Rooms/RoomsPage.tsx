bb7dd2f828e00d07fdfd1aad19e39e2f6896bf5d
Mongark <mongarkprogrammer@gmail.com>
2022-11-18 12:00:11 -0400

    Fixed bug

--- src/pages/Rooms/RoomsPage.tsx
+++ src/pages/Rooms/RoomsPage.tsx
@@ -1,12 +1,12 @@
 import { Typography } from "@material-ui/core";
-import DataTable from "../../components/DataTable";
+import DataTable from "../../components/DataTable/DataTable";
 
 function RoomsPage() {
     return(
         <div>
             <Typography variant="h4">Rooms</Typography>
 
-            <DataTable />
+            <DataTable headers={[]} body={[]} />
         </div>
     );
 }
