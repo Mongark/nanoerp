cd3d918e36f6b76e8d658de4ddb262f7175de627
Mongark <mongarkprogrammer@gmail.com>
2023-01-12 15:50:53 -0400

    Fixed typings in formatRoomData

--- src/api/format/formatRoomData.ts
+++ src/api/format/formatRoomData.ts
@@ -1,11 +1,17 @@
+interface RoomInterface {
+    __v?: number,
+    _id?: string,
+    name: string,
+}
+
 function formatRoomData(dt: any) {
     const headers = ["Name"];
-    const body: any[] = [];
+    const body: Array<Array<string>> = new Array<Array<string>>();
 
-    dt.map((item: any) => {
+    dt.map((item: RoomInterface) => {
         body.push([item.name]);
     });
-    
+
     return { headers: headers, body: body };
 }
 
